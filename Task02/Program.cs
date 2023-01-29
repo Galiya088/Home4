f329b6452b07ae7a1f903d29dfbb35e0f2cb0dcc
Galiya <safaga71@mail.ru>
2023-01-29 08:41:41 +0300

    Task02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -3,3 +3,24 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+int num = GetNum("Введите число: ");
+int GetNum(string text)
+{
+    Console.Write(text);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+int sum = SumNum(num);
+int SumNum(int num)
+{
+    int temp = num;
+    int sum = 0;
+    while (temp > 0)
+    {
+        sum = sum + temp % 10;
+        temp = temp / 10;
+    }
+    return sum;
+}
+Console.WriteLine($"{num} -> {sum}");
