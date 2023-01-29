88ad648f9df66b9543ef0e611ae1cd0c64a7af45
Galiya <safaga71@mail.ru>
2023-01-29 08:28:47 +0300

    Task01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -2,3 +2,21 @@
 // числа (A и B) и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
+int numA = GetNum("Введите число A: ");
+int numB = GetNum("Введите число B: ");
+int GetNum(string text)
+{
+    Console.Write(text);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+int pow = Pow(numA, numB);
+int Pow(int x, int y)
+{
+    int pow = 1;
+    for (int i = 0; i < y; i++) pow = pow * x;
+    return pow;
+}
+Console.WriteLine($"{numA}, {numB} -> {pow}");
+
